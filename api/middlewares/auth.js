5ee7c5493bf2e4abced02d7b24a618379e15f2ae
riad40 <riadabdelaziz40@gmail.com>
2023-01-04 22:14:20 +0100

    set authChecker middlware as endoint for verifying token

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -1,13 +1,26 @@
 const jwt = require('jsonwebtoken');
 
 const authChecker = (req, res, next) => {
+
+    try {
+        
+        // get the authorization header
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+        
+        // check if its exist
+        if (!token) return res.json(400, { loggedIn: false })
+
+        // verify token
+        const user = jwt.verify(token, process.env.JWT_SECRET)   
+        req.user = user
+        res.json(200, { loggedIn: true })
+        next()
+
+    } catch (err) {
+        next({ status: 400, error: true, message: err })
+    }
     
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (!token) return  next({ error: true, status: 400, message: "Log in first" })  
-    const user = jwt.verify(token, process.env.JWT_SECRET)   
-    req.user = user
-    next()
 }    
 
 module.exports = { authChecker }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { login, logout } = require('../controllers/authController')
 const { validate } = require('../middlewares/inputValidator')
+const { authChecker } = require('../middlewares/auth')
 
 router.post('/login', validate('login'), login)
+router.get('/isloggedin', authChecker)
 router.get('/logout', logout)
 
 module.exports = router
