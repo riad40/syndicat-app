0bbff80a2ed6693db3e1dd5e7617790d3137b4af
riad40 <riadabdelaziz40@gmail.com>
2023-01-04 16:00:12 +0100

    added cors middlware

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 require('./config/init_db').initDb()
 require('./config/config')
 const errorHandler = require('./middlewares/error-handler')
+const cors = require('cors')
 const express = require('express')
 
 const app = express()
@@ -9,6 +10,9 @@ const app = express()
 // middlwares for handling or parsing and validating incoming requests
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// cors middlware
+app.use(cors({ origin: true, credentials: true }))
 
 const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
