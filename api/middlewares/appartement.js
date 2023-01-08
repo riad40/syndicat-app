38e4f6470a6f4157ea09e1a4b2178525cc452c7e
riad40 <riadabdelaziz40@gmail.com>
2023-01-08 17:50:29 +0100

    added populate function

--- api/middlewares/appartement.js
+++ api/middlewares/appartement.js
@@ -1,14 +1,17 @@
-const Apparetement = require('../models/Appartement')
+const Apparetement = require("../models/Appartement")
+const Floor = require("../models/Floor")
 
 const getOneAppartement = (req, res, next, appa_id) => {
     Apparetement.findById(appa_id)
-        .then(data => {
+        .populate("floorNumber")
+        .then((data) => {
             req.appartement = data
             next()
         })
-        .catch(err => {
+        .catch((err) => {
+            console.log(err)
             return res.status(404).json({
-                error: 'Appartement not found'
+                error: "Appartement not found",
             })
         })
 }
